e4beac8648f9c735b117a3f3e7884f85ed96f03c
mwmyway <55584167+mwmyway@users.noreply.github.com>
2025-06-22 00:39:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git easymesh https://github.com/theosoft-git/luci-app-easymesh' >>feeds.conf.default
